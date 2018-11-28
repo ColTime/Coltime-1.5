9e3773d52d692029b75968198fe6adffae78ca70
Juan David Marulanda Paniagua <sis.informacion01@INVERTRONICA.local>
2018-11-28 07:03:06 -0500

    inicial

---
+++ ReporteEN/src/reporteen/Conexion.java
@@ -0,0 +1,62 @@
+package reporteen;
+
+import java.sql.*;
+//import javax.swing.JOptionPane;
+
+public class Conexion {
+
+    private Connection conexion;
+    static String bd;
+    static String user = "coluser";
+    static String password = "";
+    static String server;
+
+    public Conexion(int opData) {
+        bd = (opData==1?"coltime":"sgn");
+        server = "jdbc:mysql://192.168.4.1:3306/" + bd;
+    }
+
+    public void establecerConexion() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conexion = DriverManager.getConnection(server, user, password);
+//            if (conexion != null) {
+//                JOptionPane.showMessageDialog(null, "Conexion exitosa");
+//            }
+        } catch (Exception e) {
+//            JOptionPane.showMessageDialog(null, "Imposible realizar conexion con la BD" + e);
+            e.printStackTrace();
+        }
+    }
+
+    public Connection getConexion() {
+        return conexion;
+    }
+
+    public void cerrar(ResultSet rs) {
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (Exception e) {
+                System.out.print("No es posible cerrar la Conexion");
+            }
+        }
+    }
+
+    public void destruir() {
+
+        if (conexion != null) {
+
+            try {
+                conexion.close();
+            } catch (Exception e) {
+            }
+        }
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize(); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
